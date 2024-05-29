ae79022fe7498154b144a0e309ac477530fa82a7
CamiloQuezada <jos.quezadar@duocuc.cl>
2024-05-29 11:16:43 -0400

    pezar y tal

--- holaa.py
+++ holaa.py
@@ -1,6 +1,25 @@
 import os
 import time
 import msvcrt
+gentuza=[]
 while True:
     print("menusito")
     print("1) Inicio sesion\n2) Registrar usuario\n3)Eliminar usuario\n4) Salir")
+    opc = int(input("Eliga opción: "))
+    if opc==1:
+        pass
+    elif opc==2:
+        print("Registro :3")
+        non = input("Ingrese su nombre de usuario: ")
+        con = input("Ingrese su contraseña: ")
+        usuario ={
+            "Nombre": non,
+            "Contrasena": con,}
+        gentuza.append(usuario)
+        print("Wena papu estas registrado como un papu")
+    elif opc==3:
+        pass
+    else:
+        print("CHAO TONTO")
+        time.sleep(3)
+        os.system('cls')
